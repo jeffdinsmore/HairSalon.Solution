bfe2223ac96a15efd64b369ad533d62d05c6557b
JeffDinsmore <hello@jeffdinsmore.com>
2020-10-09 12:42:03 -0700

    testing pages and found an error for clients not saving stylist Id

--- HairSalon/Controllers/StylistsControllers.cs
+++ HairSalon/Controllers/StylistsControllers.cs
@@ -3,12 +3,12 @@ using HairSalon.Models;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace HairSalon.Controllers
 {
   public class StylistsController : Controller
   {
-
     private readonly HairSalonContext _db;
 
     public StylistsController(HairSalonContext db)
